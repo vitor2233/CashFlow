e7ba16e54e0f450d1b7c640ff5143d545ae9af92
vitor2233 <vitorguilherme2233@gmail.com>
2024-06-20 19:37:01 -0300

    Add UserReadonly mock in tests

---
+++ tests/CommonTestUtilities/Repositories/UserReadOnlyRepositoryBuilder.cs
@@ -0,0 +1,21 @@
+
+using CashFlow.Domain.Repositories.Users;
+using Moq;
+
+namespace CommonTestUtilities.Repositories;
+public class UserReadOnlyRepositoryBuilder
+{
+    private readonly Mock<IUserReadOnlyRepository> _repository;
+
+    public UserReadOnlyRepositoryBuilder()
+    {
+        _repository = new Mock<IUserReadOnlyRepository>();
+    }
+
+    public void ExistActiveUserWithEmail(string email)
+    {
+        _repository.Setup(r => r.ExistActiveUserWithEmail(email)).ReturnsAsync(true);
+    }
+
+    public IUserReadOnlyRepository Build() => _repository.Object;
+}
--- tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
+++ tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Users.Register;
+using CashFlow.Exception.ExceptionBase;
 using CommonTestUtilities.Cryptography;
 using CommonTestUtilities.Mapper;
 using CommonTestUtilities.Repositories;
@@ -20,15 +21,46 @@ public class RegisterUserUseCaseTest
 
         result.Should().NotBeNull();
         result.Name.Should().Be(request.Name);
+        result.Token.Should().NotBeNullOrWhiteSpace();
     }
 
-    private RegisterUserUseCase CreateUseCase()
+    [Fact]
+    public async Task Error_Name_Empty()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        var useCase = CreateUseCase();
+        var act = async ()=> await useCase.Execute(request);
+
+        var result = await act.Should().ThrowAsync<ErrorOnValidationException>();
+        result.Where(e=> e.GetErrors().Count == 1 && e.GetErrors().Contains("Name is required."));
+    }
+
+    [Fact]
+    public async Task Error_Email_Already_Exists()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        var useCase = CreateUseCase(request.Email);
+        var act = async ()=> await useCase.Execute(request);
+
+        var result = await act.Should().ThrowAsync<ErrorOnValidationException>();
+        result.Where(e=> e.GetErrors().Count == 1 && e.GetErrors().Contains("Email já está sendo utilizado"));
+    }
+
+    private RegisterUserUseCase CreateUseCase(string? email = null)
     {
         var mapper = MapperBuilder.Build();
         var unitOfWork = UnitOfWorkBuilder.Build();
         var writeOnlyRepository = UserWriteOnlyRepositoryBuilder.Build();
         var passwordEncripter = PasswordEncripterBuilder.Build();
         var jwtTokenGenerator = JwtTokenGeneratorBuilder.Build();
-        return new RegisterUserUseCase(null, null, null, null, null, null);
+        var readOnlyUserRepository = new UserReadOnlyRepositoryBuilder();
+        if(!string.IsNullOrWhiteSpace(email))
+        {
+            readOnlyUserRepository.ExistActiveUserWithEmail(email);
+        }
+        return new RegisterUserUseCase(mapper, passwordEncripter, readOnlyUserRepository.Build(), writeOnlyRepository, unitOfWork, jwtTokenGenerator);
     }
 }
